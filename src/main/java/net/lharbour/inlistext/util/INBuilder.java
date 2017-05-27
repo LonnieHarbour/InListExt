9e35343d745a6c53cff713ada516c9c66904193a
Lonnie Harbour <lonnie.harbour@lharbour.net>
2017-05-26 21:18:01 -0400

    Incremental updates.

--- src/main/java/net/lharbour/inlistext/util/INBuilder.java
+++ src/main/java/net/lharbour/inlistext/util/INBuilder.java
@@ -1,13 +1,13 @@
 package net.lharbour.inlistext.util;
 
 import java.util.Collection;
-import java.util.logging.Handler;
 
+import net.lharbour.inlistext.enums.Handler;
 import net.lharbour.inlistext.exceptions.NoValueINListException;
 
 public class INBuilder {
 
-	public static String build(String key, Handler handler, final Collection<?> parms) {
+	public static String build(String name, Handler handler, final Collection<?> parms) {
 		
 		if (parms==null || parms.size()==0) {
 			throw new NoValueINListException("Null or zero length parameter");
@@ -15,7 +15,14 @@ public class INBuilder {
 		
 		StringBuilder sb = new StringBuilder();
 
-		
+		for (int x=0;x<parms.size();x++) {
+			sb.append(sb.length()==0 ? "" : ",");
+			sb.append("#{").
+			   append(name).
+			   append(", typeHandler=").
+			   append(handler.key()).
+			   append("}");
+		}
 		
 		return sb.toString();
 	}
